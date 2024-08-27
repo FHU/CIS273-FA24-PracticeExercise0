f357083b342b90a7bda43351df94a29ebf462738
Kenan Casey <kenancasey@gmail.com>
2024-08-27 08:50:24 -0500

    initial commit

---
+++ PracticeExercise0/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace PracticeExercise0;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+ 
+    }
+
+    /***
+     * Write a method that will take a string and determine if the 
+     * string is a palindrome (the same forwards and backward). 
+     * Ignore the case and all spaces. 
+    */
+    public static bool IsPalindrome(string s)
+    {
+        // clean up
+       return true;
+    }
+
+    private static string Reverse(string original)
+    {
+        return original;
+    }
+
+
+    /***
+     * Write a method that accepts an integer (positive or negative) and 
+     * returns the “reversed” number, but retains the sign. 
+     */
+    public static int ReverseInt(int i)
+    {
+       return i;
+    }
+
+    // This doesn't handle negative numbers correctly.
+    public static int ReverseInt2(int i)
+    {
+        return i;
+    }
+
+
+    /***
+     * Write a function that will accept a string of alphabetic characters 
+     * and determine if all the characters are unique (i.e., no characters 
+     * are duplicated). 
+     * This comparison should be case INsensitive and ignore spaces.
+     */
+    public static bool IsUnique(string s)
+    {
+        return true;
+    }
+
+
+    /***
+     * A Neil number of m digits is an integer such that the sum of its 
+     * digits raised to the mth power is equal to the number itself. For 
+     * example, 371 is a Neil number since 33 + 73 + 13 = 371. Write the 
+     * following method to test an integer for this property.
+     */
+    public static bool IsNeilNumber(int i)
+    {
+        return true;
+    }
+
+
+    /***
+     * 
+     * Write a function that converts phone numbers from this format: 
+     * "7three1-6zero8-3one35" 
+     * to the standard format:
+     * "731-608-3135"
+     */
+    public static string Convert(string s)
+    {
+        return "";
+    }
+
+}
+
